100aac78adc9c993f2180ae3c80000be86fe6194
Gary Alan Ridgway <gary-ridgway@uiowa.edu>
2020-07-31 07:54:58 -0500

    card headline and button component without scss or js

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <uidsCard />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import uidsCard from './components/uidsCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    uidsCard,
   }
 }
 </script>
