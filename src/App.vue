7e7e485398c3f2eaad685e4fd83a74e8010a949b
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2020-08-05 17:38:50 -0500

    Updated logic to allow for adding additional classes to the last hero block.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="card__wrapper">
-    <template v-for="area in areas_of_study">
+    <template v-for="(area, index) in areas_of_study">
       <hero :headline="area.headline"
             :image="area.image"
-            class="hero--row hero--solid-opacity hero--large hero--vertical-bottom hero--horizontal-left"
+            :class="getHeroClasses(index)"
       >
       </hero>
     </template>
@@ -30,13 +30,31 @@ export default {
           headline: 'Education',
           image: 'https://uids.brand.uiowa.edu/branches/feature_admissions_viewbook/assets/images/viewbook/areas/2.jpg',
           link: 'https://admissions.uiowa.edu/academics/edu',
+        },
+        {
+          headline: '',
+          image: '',
+          link: 'https://admissions.uiowa.edu/academics/undergraduate-areas-of-study',
         }
       ],
     }
   },
+  methods: {
+    getHeroClasses(index) {
+      console.log('index', index);
+      console.log('count', this.areas_of_study.length);
+      let classes = 'hero--solid-opacity hero--large hero--row hero--vertical-bottom hero--horizontal-left';
+      if (index+1 >= this.areas_of_study.length) {
+        classes += ' bg-pattern--brain-black';
+      }
+      return classes;
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+  $imgpath: '../node_modules/@uiowa/uids/src/assets/images';
+  @import '../node_modules/@uiowa/uids/src/components/colors/colors.scss';
   @import '../node_modules/@uiowa/uids/src/components/layouts/viewbook/viewbook';
 </style>
