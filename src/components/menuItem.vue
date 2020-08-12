d5876a07dc2c91a0791968fd1ff98f2027b1e3aa
Gary Alan Ridgway <gary-ridgway@uiowa.edu>
2020-08-12 15:20:33 -0500

    cleanup

--- src/components/menuItem.vue
+++ src/components/menuItem.vue
@@ -1,6 +1,6 @@
 <template>
 <li class="menu-item">
-  <a :href="url" :class="item_class"><span class="link-text">{{text}}</span></a>
+  <a :href="url" :class="item_class">{{text}}</a>
   <!-- We would render submenu Items here -->
   <!-- {% if item.children %}
     {# If there is a child menu under this link, render it. #}
@@ -17,18 +17,3 @@ export default {
 
 <style lang="scss">
 </style>
-
-link_bttn:
-    items:
-      - text: Apply Now
-        url: https://admissions.uiowa.edu/apply/first-year-student-application-process
-        item_class: bttn bttn--outline bttn--tertiary bttn--transparent bttn--caps bttn--apply
-      - text:  Schedule a Visit
-        url: https://admissions.uiowa.edu/visit-campus
-        item_class: bttn bttn--outline bttn--tertiary bttn--transparent bttn--caps bttn--visit
-      - text: Contact Us
-        url: https://admissions.uiowa.edu/contact-us
-        item_class: bttn bttn--outline bttn--tertiary bttn--transparent bttn--caps bttn--ask
-      - text: Request Information
-        url: https://www.maui.uiowa.edu/maui/pub/admissions/webinquiry/undergraduate.page
-        item_class: bttn bttn--outline bttn--tertiary bttn--transparent bttn--caps bttn--connect
