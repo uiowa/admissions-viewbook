254ed274f112d18d8e60c8201872d0db658bb357
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2022-10-05 13:41:00 -0500

    Updates per #68.

--- src/components/Aerial.vue
+++ src/components/Aerial.vue
@@ -53,12 +53,12 @@ export default {
       aerial_image: '/images/viewbook/sections/12.jpg',
       grid_title_1: 'World-class hospital',
       grid_sub_1: ' A nationally ranked, comprehensive medical center is just a short walk away.',
-      grid_title_2: 'Best college town',
-      grid_sub_2: 'Named the #1 Best College Town by reviews.org, Iowa City seamlessly blends a vibrant downtown with the heart of campus.',
+      grid_title_2: 'Top college town',
+      grid_sub_2: 'Named a top 10 college town by U S A Today, Iowa City seamlessly blends the heart of campus with a vibrant downtown that offers a variety of annual festivals, block parties, author readings, and workshops throughout the year.',
       grid_title_3: 'Easy to get around',
       grid_sub_3: ' Whether you like to walk, bike, or take the free bus system, residence halls are closely located to the center of campus and downtown Iowa City.',
       grid_title_4: 'Big ten athletics',
-      grid_sub_4: 'Seating nearly 70,000, Kinnick Stadium is home to Hawkeye Football, just one of the 21 NCAA teams at Iowa.'
+      grid_sub_4: 'Seating nearly 70,000, Kinnick Stadium is home to Hawkeye Football, just one of the 20 NCAA teams at Iowa.'
     }
   }
 }
